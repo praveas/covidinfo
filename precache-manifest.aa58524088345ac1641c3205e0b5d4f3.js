f1b2b4d0754b1ac42fad0900fc3750e1d252917a
Prabesh Pokharel <praveas@gmail.com>
2020-08-02 00:26:54 +1000

    Updates

---
+++ precache-manifest.aa58524088345ac1641c3205e0b5d4f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6467e67b6439e2406122da778ddb3150",
+    "url": "/covidinfo/index.html"
+  },
+  {
+    "revision": "df7ee7955d1de513b261",
+    "url": "/covidinfo/static/css/main.8f2f6066.chunk.css"
+  },
+  {
+    "revision": "5a1ac9c9bc74845fb65b",
+    "url": "/covidinfo/static/js/2.e1750a04.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidinfo/static/js/2.e1750a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7ee7955d1de513b261",
+    "url": "/covidinfo/static/js/main.f4cc8f1f.chunk.js"
+  },
+  {
+    "revision": "2aeac8788e239c18fc98",
+    "url": "/covidinfo/static/js/runtime-main.f18a8a1f.js"
+  },
+  {
+    "revision": "a4b0fc75732fd9720aba478365dffc2f",
+    "url": "/covidinfo/static/media/global-virus-scaled.a4b0fc75.jpg"
+  },
+  {
+    "revision": "fd844e861be1f670725512ed68d98151",
+    "url": "/covidinfo/static/media/logo.fd844e86.webp"
+  }
+]);
